9800079094699b97625bc88ab20e5feea0e5964f
Ly <h_ly70268@tss-hr420-dm17.fc.ca>
2018-05-30 13:54:31 -0400

    add js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function() {
+	"use strict";
+	console.log("file connected");
+// Single line comment //
+/* Multi line comment */
+ 
+
+var words = "This is a string of letters"; /* you can put anything inside a variable */
+var num = 1;
+console.log(words);
+console.log("Number of times of visited "+num) ;
+var teams = ["Knights","Caps","Leafs"];
+console.log(teams[0]); // 1st position of an array is 0//
+
+function i_am_a_function () {
+	consolelog("From i_am_a_function()");
+} 
+
+window.addEventListener("load", i_am_a_function, false);
+
+
+
+
+})();
